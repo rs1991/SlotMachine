321b612ed65da89ed5fa6157dd1bb1f3c920b33a
Rida Serroukh <rida.serroukh@gmail.com>
2022-04-05 18:35:11 +0100

    random number generator and 2D array created

--- SlotMachine/Program.cs
+++ SlotMachine/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 
-namespace SlotMachine 
+namespace SlotMachine
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the slot machine!");
+
+            //Random number generator
+            Random random = new Random();
+            int num1 = random.Next(9);
+            int num2 = random.Next(9);
+            int num3 = random.Next(9);
+            int num4 = random.Next(9);
+            int num5 = random.Next(9);
+            int num6 = random.Next(9);
+            int num7 = random.Next(9);
+            int num8 = random.Next(9);
+            int num9 = random.Next(9);
+
+            int[,] array = new int[3, 3] { 
+                { num1, num2, num3 }, 
+                { num4, num5, num6 }, 
+                { num7, num7, num8 } }; 
+
         }
+
     }
 }
