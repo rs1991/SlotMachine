b712920bbba6d636c30333d91ee522da31b25c0a
Rida Serroukh <rida.serroukh@gmail.com>
2022-04-05 17:47:11 +0100

    Add project files.

---
+++ SlotMachine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SlotMachine 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the slot machine!");
+        }
+    }
+}
