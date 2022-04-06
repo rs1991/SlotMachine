e90d51f0b538858dac8a89c984e7e8ebe533b193
Rida Serroukh <rida.serroukh@gmail.com>
2022-04-06 23:42:09 +0100

    Added the switch statement to allow the user to select which lines they would like to play

--- SlotMachine/Program.cs
+++ SlotMachine/Program.cs
@@ -6,6 +6,9 @@ namespace SlotMachine
     {
         static void Main(string[] args)
         {
+            double Wager = 0, Winnings = 0;
+            string TopLine, CenterLine, AllHorizontal, AllVertical, Diagonal, LineToPlay;
+
             Console.WriteLine("Welcome to the slot machine!");
 
             //Random number generator
@@ -20,10 +23,56 @@ namespace SlotMachine
             int num8 = random.Next(9);
             int num9 = random.Next(9);
 
-            int[,] array = new int[3, 3] { 
-                { num1, num2, num3 }, 
-                { num4, num5, num6 }, 
-                { num7, num7, num8 } }; 
+            int[,] rows = new int[3, 3] {
+                { num1, num2, num3 },
+                { num4, num5, num6 },
+                { num7, num7, num8 } };
+
+            Console.WriteLine("Place your wager: ");
+            Wager = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Select which line you would like to play: 'T' Top line, 'C' Center line, 'AH' all horizontal lines, 'AV' All Vertical lines, 'D' Diagonal lines");
+            LineToPlay = Console.ReadLine().ToUpper();
+
+
+            switch (LineToPlay)
+            {
+                case "T":
+                    Console.WriteLine("Play the top line");
+                    break;
+
+                case "C":
+                    Console.WriteLine("Play the center line");
+                    break;
+
+                case "AH":
+                    Console.WriteLine("Play all horizontal lines");
+                    break;
+
+                case "AV":
+                    Console.WriteLine("Play all vertical lines");
+                    break;
+
+                case "D":
+                    Console.WriteLine("Play diagonal lines");
+                    break;
+
+                    
+
+
+
+
+            }
+
+            /*if(LineToPlay == "T")
+            {
+                Console.WriteLine("Play the top line");
+            }
+            if (LineToPlay == "C")
+            {
+                Console.WriteLine("Play the center line");
+            }
+            */
 
         }
 
