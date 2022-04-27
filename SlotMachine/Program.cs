59750d2755f6a050834ef15d6287fc5b11be6000
Rida Serroukh <rida.serroukh@gmail.com>
2022-04-27 22:45:33 +0100

    Foor loops for the different play options fixed

--- SlotMachine/Program.cs
+++ SlotMachine/Program.cs
@@ -21,7 +21,7 @@ namespace SlotMachine
 
             while (PlayAgain)
             {
-
+                
                 Console.WriteLine("----------------");
                 Console.Write("Place your wager: $ ");
                 var UserWager = Console.ReadLine();
@@ -42,74 +42,85 @@ namespace SlotMachine
                     }
                 }
 
-
-                Console.WriteLine("Select which line you would like to play: 'T' Top line, 'C' Center line, 'AH' all horizontal lines, 'AV' All Vertical lines, 'D' Diagonal lines: ");
-                var LineToPlay = Console.ReadLine().ToUpper();
-
-                switch (LineToPlay)
+                bool InvalidInput = true;
+                while (InvalidInput)
                 {
-                    case "T":
-                        for (int row = 0; row > 3; row++)
-                        {
-                            if (rows[row, 0] == rows[row, 1] &&
-                                rows[row, 0] == rows[row, 2])
+                    Console.WriteLine("Select which line you would like to play: 'T' Top line, 'C' Center line, 'AH' all horizontal lines, 'AV' All Vertical lines, 'D' Diagonal lines: ");
+                    var LineToPlay = Console.ReadLine().ToUpper();
+                    switch (LineToPlay)
+                    {
+                        case "T":
+                            if (rows[0, 0] == rows[0, 1] &&
+                                rows[0, 1] == rows[0, 2])
                             {
                                 //do something (add 2 coins to your winning)
                                 Winnings = (Wager * Odds) + 1;
                             }
-                        }
-                        break;
-                    case "C":
-                        for (int row = 0; row < 3; row++)
-                        {
-                            if (rows[row, 0] == rows[row, 1] &&
-                                rows[row, 1] == rows[row, 2])
+                            InvalidInput = false;
+                            break;
+                        case "C":
+                            if (rows[1, 0] == rows[1, 1] &&
+                                rows[1, 1] == rows[1, 2])
                             {
                                 Winnings = (Wager * Odds) + 1;
                             }
-                        }
-                        break;
-                    case "AH":
-                        for (int row = 0; row < 3; row++)
-                        {
-                            if (rows[row, 0] == rows[row, 1] &&
-                                rows[row, 1] == rows[row, 2])
+                            InvalidInput = false;
+                            break;
+                        case "AH":
+                            for (int row = 0; row < 3; row++)
                             {
-                                //do something (add 2 coins to your winning)
-                                Winnings = (Wager * Odds) + 1;
+                                if (rows[row, 0] == rows[row, 1] &&
+                                    rows[row, 1] == rows[row, 2])
+                                {
+                                    //do something (add 2 coins to your winning)
+                                    Winnings = (Wager * Odds) + 1;
+                                }
                             }
-                        }
-                        break;
-                    case "AV":
-                        for (int col = 0; col < 3; col++)
-                        {
-                            if (rows[col, 0] == rows[col, 0] &&
-                                rows[col, 0] == rows[col, 0])
+                            InvalidInput = false;
+                            break;
+                        case "AV":
+                            for (int i = 0; i < 3; i++)
                             {
-                                //do something (add 2 coins to your winning)
-                                Winnings = (Wager * Odds) + 2;
+                                for (int j = 0; j < 3; j++)
+                                {
+                                    if (rows[0, 0] == rows[1, 0] &&
+                                        rows[0, 1] == rows[1, 1] &&
+                                        rows[1, 1] == rows[2, 1] &&
+                                        rows[0, 2] == rows[1, 2] &&
+                                        rows[1, 2] == rows[2, 2])
+                                    {
+                                        //do something (add 2 coins to your winning)
+                                        Winnings = (Wager * Odds) + 2;
+                                    }
+                                }
                             }
-                        }
-                        break;
-                    case "D":
-                        for (int row = 0; row < 3; row++)
-                        {
-                            if (rows[row, 0] == rows[row, 1] &&
-                                rows[row, 1] == rows[row, 2])
+                            InvalidInput = false;
+                            break;
+                        case "D":
+                            for (int i = 0; i < 3; i++)
                             {
-                                //do something (add 2 coins to your winning)
-                                Winnings = (Wager * Odds) + 1;
+                                for (int j = 0; j < 3; j++)
+                                {
+                                    if (rows[0, 0] == rows[1, 1] &&
+                                        rows[1, 1] == rows[2, 2]||
+                                        rows[2,0] == rows[1, 1]&&
+                                        rows[1,1] == rows[0,2])
+                                    {
+                                        Winnings = (Wager * Odds) + 1;
+                                    }
+                                }
                             }
-                        }
-                        break;
-                    default:
-                        Console.WriteLine("please enter a valid choice");
-                        break;
-
-
-
+                            InvalidInput = false;
+                            break;
+                        default:
+                            InvalidInput = true;
+                            break;
+                    }
+                    if (InvalidInput)
+                    {
+                        Console.WriteLine("Valid input please");
+                    }
                 }
-
 
                 //display grid and winnings
                 for (int row = 0; row < 3; row++)
@@ -138,10 +149,8 @@ namespace SlotMachine
                     PlayAgain = false;
                 }
             }
-
-
-                Console.WriteLine("Thanks for playing");
-                Console.ReadKey();
-            }
+            Console.WriteLine("Thanks for playing");
+            Console.ReadKey();
         }
     }
+}
