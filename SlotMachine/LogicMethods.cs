734233d5d32b76b594971550d0d72b2ffc161d71
Rida Serroukh <rida.serroukh@gmail.com>
2022-05-23 20:34:37 +0100

    Added XML comments to the LogicMethods. Unnecessary assignment to total removed.

--- SlotMachine/LogicMethods.cs
+++ SlotMachine/LogicMethods.cs
@@ -8,27 +8,42 @@ namespace SlotMachine
 {
     public static class LogicMethods
     {
-
+        /// <summary>
+        /// Adds up the winnings of a single line bet
+        /// </summary>
+        /// <param name="Bet">The bet that the user places</param>
+        /// <param name="Odd">The odds added to the equation if user wins</param>
+        /// <returns>The winnings of a single line bet</returns>
         public static double CalculateSingleLineWinnings(double Bet, double Odd)
         {
-            double Total = 0;
+            double Total;
             Total = Bet * Odd + 1;
             return Total;
         }
 
-        public static double CalculateOtherWinnings(double Bet, double Odd, double Trio)
+        /// <summary>
+        /// Calculate the winnings for when three lines are played
+        /// </summary>
+        /// <param name="Bet">The bet that the user places</param>
+        /// <param name="Odd">The odds added to the equation if user wins</param>
+        /// <param name="Trio">Triple winnings for when three lines have matching numbers</param>
+        /// <returns>The winnings for when three lines are played</returns>
+        public static double CalculateTripleLineWinnings(double Bet, double Odd, double Trio)
         {
-            double Total = 0;
+            double Total;
             Total = Bet * Odd * Trio + 1;
             return Total;
         }
 
-
+        /// <summary>
+        /// Generates the random grid
+        /// </summary>
+        /// <returns></returns>
         public static int[,] GenerateGrid()
         {
             //Random number generator
             Random rng = new Random();
-            //int max = 9;
+            int max = 9;
             //Grid array
             int[,] Grid1 = new int[3, 3];
             //Grid to generate random numbers
@@ -36,13 +51,20 @@ namespace SlotMachine
             {
                 for (int col = 0; col < 3; col++)
                 {
-                    //Grid1[row, col] = rng.Next(max + 1);
+                    Grid1[row, col] = rng.Next(max + 1);
                 }
             }
-
             return Grid1;
         }
 
+        /// <summary>
+        /// Calculates winnings for individual rows
+        /// </summary>
+        /// <param name="Odds">The odds added to the equation if user wins</param>
+        /// <param name="Wager">The wager that the user places</param>
+        /// <param name="Grid">If statement checks through the grid to see if numbers match</param>
+        /// <param name="row"></param>
+        /// <returns>Calculates the winnings for the individual rows</returns>
         public static double CalculateRowWinnings(double Odds, double Wager, int[,] Grid, int row)
         {
             double Winnings = 0;
@@ -53,6 +75,14 @@ namespace SlotMachine
             return Winnings;
         }
 
+        /// <summary>
+        /// Calculates the horizontal winnings  
+        /// </summary>
+        /// <param name="Odds">Odds used to calculate the winnings</param>
+        /// <param name="Wager">Wager placed by user</param>
+        /// <param name="Grid">If statement checks through the grid to see if a winning combination is found</param>
+        /// <param name="TrioCombo">If three lines match then winnings are tripled</param>
+        /// <returns>Winnings for the horizontal lines played</returns>
         public static double CalculateHorizontalWinnings(double Odds, double Wager, int[,] Grid, double TrioCombo)
         {
             int WinningRowcnt = 0;
@@ -67,11 +97,19 @@ namespace SlotMachine
             double Winnings = 0;
             if (WinningRowcnt == 3)
             {
-                Winnings = LogicMethods.CalculateOtherWinnings(Wager, Odds, TrioCombo);
+                Winnings = LogicMethods.CalculateTripleLineWinnings(Wager, Odds, TrioCombo);
             }
             return Winnings;
         }
 
+        /// <summary>
+        /// Calculates winnings for vertical lines  
+        /// </summary>
+        /// <param name="Odds">Odds added to the winnings calculation</param>
+        /// <param name="Wager">User wager</param>
+        /// <param name="Grid">If statement checks through the grid to see if a winning combination is found</param>
+        /// <param name="TrioCombo"></param>
+        /// <returns>Vertical winnings</returns>
         public static double CalculateAllVerticalWinnings(double Odds, double Wager, int[,] Grid, double TrioCombo)
         {
             int WinningRowcnt = 0;
@@ -86,11 +124,19 @@ namespace SlotMachine
             double Winnings = 0;
             if (WinningRowcnt == 3)
             {
-                Winnings = LogicMethods.CalculateOtherWinnings(Wager, Odds, TrioCombo);
+                Winnings = LogicMethods.CalculateTripleLineWinnings(Wager, Odds, TrioCombo);
             }
             return Winnings;
         }
 
+        /// <summary>
+        /// Winning calculation for diagonal lines
+        /// </summary>
+        /// <param name="Odds"></param>
+        /// <param name="Wager"></param>
+        /// <param name="Grid"></param>
+        /// <param name="TrioCombo"></param>
+        /// <returns>Winnings from when the diagonal lines are played</returns>
         public static double CalculateDiagWinnings(double Odds, double Wager, int[,] Grid, double TrioCombo)
         {
             int WinningDiagCnt = 0;
@@ -106,7 +152,7 @@ namespace SlotMachine
             double Winnings = 0;
             if (WinningDiagCnt == 3)
             {
-                Winnings = LogicMethods.CalculateOtherWinnings(Wager, Odds, TrioCombo);
+                Winnings = LogicMethods.CalculateTripleLineWinnings(Wager, Odds, TrioCombo);
             }
             return Winnings;
         }
